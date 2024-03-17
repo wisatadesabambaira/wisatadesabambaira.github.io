0e48593ad47dfb8cd94ffb46cb24e7fc0c28cd9b
Muhammad Awan Ardy Firmansyah <muhammadawan46@gmail.com>
2024-03-18 00:22:32 +0800

    penambahan dropdown

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function toggleMenu() {
 function closeMenu() {
   document.getElementById("overlay-menu").style.height = "0%";
 }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var wisataDesaBtn = document.getElementById("wisataDesaBtn");
+//   var wisataDesaDropdown = document.getElementById("wisataDesaDropdown");
+
+//   wisataDesaBtn.addEventListener("click", function () {
+//     var isHidden = wisataDesaDropdown.style.display === "none";
+//     wisataDesaDropdown.style.display = isHidden ? "block" : "none";
+//   });
+// });
+
+function toggleDropdown(event) {
+  const dropdownContent = event.currentTarget.nextElementSibling;
+  if (dropdownContent.classList.contains("clicked")) {
+    dropdownContent.classList.remove("clicked");
+  } else {
+    dropdownContent.classList.add("clicked");
+  }
+}
+
+// Tambahkan event listener untuk setiap dropdown
+document.querySelectorAll(".dropdown > a").forEach((item) => {
+  item.addEventListener("click", toggleDropdown);
+});
